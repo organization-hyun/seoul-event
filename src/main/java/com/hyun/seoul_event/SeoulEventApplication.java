c0324eded6a9bf04e47d360b8f8c2ed8508c13d8
hyunjuuun <hyunjun86120@gmail.com>
2024-10-17 22:41:51 +0900

    프로젝트 기본 세팅

---
+++ src/main/java/com/hyun/seoul_event/SeoulEventApplication.java
@@ -0,0 +1,13 @@
+package com.hyun.seoul_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyun/seoul_event/SeoulEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyun.seoul_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
