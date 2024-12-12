bfeef4b43d7d72e7c3c1a34f776e839ff2938c7f
Hg-lab <siregy1222@gmail.com>
2024-12-12 22:29:16 +0900

    Spring Batch 컨트롤러

--- src/main/java/com/hyun/seoul_event/batch/seouleventif/controller/SeoulEventBatchController.java
+++ src/main/java/com/hyun/seoul_event/batch/seouleventif/controller/SeoulEventBatchController.java
@@ -2,6 +2,10 @@ package com.hyun.seoul_event.batch.seouleventif.controller;
 
 import com.hyun.seoul_event.batch.seouleventif.service.SeoulEventBatchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SeoulEventBatchController {
 
-
     private final SeoulEventBatchService seoulEventBatchService;
+
+    private final JobLauncher jobLauncher;
+    private final Job myJob;
 
     @RequestMapping("/fetch-and-save-all-events/on-demand")
     public void fetchAndSaveAllEventsOnDemand() {
         seoulEventBatchService.fetchAndSaveAllSeoulEvents();
     }
 
+    @RequestMapping("/on-demand")
+    public void onDemand() {
+        try {
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addLong("timestamp", System.currentTimeMillis()) // 매번 고유한 파라미터 추가
+                    .toJobParameters();
+            jobLauncher.run(myJob, jobParameters);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
